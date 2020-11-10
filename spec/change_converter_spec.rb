4cce2effbfee8fdab54d2cb021abe096c5737be3
Tim <timcpb@hotmail.com>
2020-11-10 15:53:34 +0000

    red test - 'returns change below £1'

--- spec/change_converter_spec.rb
+++ spec/change_converter_spec.rb
@@ -17,4 +17,9 @@ describe 'change_converter' do
         expect(change_converter.convert(60)).to eq(["£50", "£10"])
     end
 
+    it('returns change below £1') do
+        expect(change_converter.convert(16.30)).to eq(["£10", "£5", "£1", "20p", "10p"])
+        expect(change_converter.convert(76.81)).to eq(["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"])
+    end
+
 end
