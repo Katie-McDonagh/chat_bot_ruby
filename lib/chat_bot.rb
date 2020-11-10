b6374c76d4e9f5c1faf0f472ae5dd7b20bce6b3e
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-10 12:46:36 +0000

    progress

--- lib/chat_bot.rb
+++ lib/chat_bot.rb
@@ -1,5 +1,34 @@
 def chat_bot
 
-  print "hello I'm your parrot, what do you want me to repeat... "
+print  "hello I'm your parrot, what do you want me to repeat... "
+
+print "curtain"
 
 end
+
+class Chat_bot 
+
+  def welcome
+    "hello im your echo"
+  end
+
+  def user_response
+    gets.chomp
+  end
+
+  def get_time
+  parse the get_time
+  end 
+
+  def return statment
+string interpolation 'chat_bot.welcome, user response' unless user_response == 'exit' 
+  end
+
+  while userResponse != exit
+
+    return user response 
+
+  end
+
+  
+
--- spec/chat_bot_spec.rb
+++ spec/chat_bot_spec.rb
@@ -6,11 +6,12 @@ describe 'chat_bot' do
   it('asks a user for input') do
      expect{ chat_bot }.to output("hello I'm your parrot, what do you want me to repeat... ").to_stdout 
   end
-  # specify { expect { print('foo') }.to output('foo').to_stdout }
 
-  # it('prints a users out put back to them') do
-
-  # end
+  
+  it('prints a users out put back to them') do  
+    user_response = 'curtain'
+    expect{ chat_bot }.to output(user_response).to_stdout 
+    end
 
   # it('repeats the users output within a string') do
 
