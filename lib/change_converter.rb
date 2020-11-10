d8a152ab9777b57c7c2f1c843ec40408b3c560d0
Tim <timcpb@hotmail.com>
2020-11-10 15:24:15 +0000

    green test - returns [£50, £20] when given 70

--- lib/change_converter.rb
+++ lib/change_converter.rb
@@ -1,7 +1,20 @@
 class ChangeConverter
     
     def convert(num)
-        ["£#{num}"]
+        # ["£#{num}"]
+        results_array = []
+        while num > 0 do
+            if num >= 50
+                results_array.push("£50")
+                num -= 50
+            elsif num >= 20 && num < 50
+                results_array.push("£20")
+                num -= 20
+            end
+        end
+
+    results_array
+
     end
 
 end
