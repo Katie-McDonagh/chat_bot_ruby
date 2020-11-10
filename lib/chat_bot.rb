7a357a2b666f56f723fa5a033481b8f1d22d7540
Tim <timcpb@hotmail.com>
2020-11-10 14:41:59 +0000

    green test - 'asks a user for input'

--- lib/chat_bot.rb
+++ lib/chat_bot.rb
@@ -1,34 +1,10 @@
-def chat_bot
+#!/usr/bin/env ruby
 
-print  "hello I'm your parrot, what do you want me to repeat... "
-
-print "curtain"
-
-end
-
-class Chat_bot 
-
-  def welcome
-    "hello im your echo"
-  end
-
-  def user_response
-    gets.chomp
-  end
-
-  def get_time
-  parse the get_time
-  end 
-
-  def return statment
-string interpolation 'chat_bot.welcome, user response' unless user_response == 'exit' 
-  end
-
-  while userResponse != exit
-
-    return user response 
-
-  end
 
   
 
+puts "hello I'm your parrot, what do you want me to repeat... "
+
+user_response = gets.chomp
+
+puts "you said #{user_response}"
--- spec/chat_bot_spec.rb
+++ spec/chat_bot_spec.rb
@@ -4,17 +4,30 @@ describe 'chat_bot' do
 
 
   it('asks a user for input') do
-     expect{ chat_bot }.to output("hello I'm your parrot, what do you want me to repeat... ").to_stdout 
+    expect{ system %(ruby lib/chat_bot.rb) }
+    .to output(a_string_including("hello I'm your parrot, what do you want me to repeat... "))
+    .to_stdout_from_any_process 
   end
 
-  
-  it('prints a users out put back to them') do  
-    user_response = 'curtain'
-    expect{ chat_bot }.to output(user_response).to_stdout 
-    end
-
-  # it('repeats the users output within a string') do
-
+  # it('asks a user for input') do
+  #   system %(ruby lib/chat_bot.rb)
+  #   expect{ system %(Yoooo) }
+  #   .to output(a_string_including("Yoooo"))
+  #   .to_stdout_from_any_process 
   # end
 
+  
+  # it('prints a users out put back to them') do  
+  #   user_response = 'curtain'
+  #   expect{ chat_bot }.to output(user_response).to_stdout 
+  #   end
+
+  # # it('repeats the users output within a string') do
+
+  # # end
+
 end
+
+# expect { system %(./lib/chat_bot.rb) }
+#  .to output(a_string_including("hello I'm your parrot, what do you want me to repeat... "))
+#  .to_stdout_from_any_process
