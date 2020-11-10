43f86de26454a7035872fdad27cb3749b28e82cf
Tim <timcpb@hotmail.com>
2020-11-10 15:47:33 +0000

    green test - pushes all change amounts above £1 into the results_array

--- lib/change_converter.rb
+++ lib/change_converter.rb
@@ -2,7 +2,7 @@ class ChangeConverter
     
     def convert(num)
         results_array = []
-        change_denominations = [50, 20]
+        change_denominations = [50, 20, 10, 5, 2, 1]
         while num > 0 do
             change_denominations.each do |option|
                 if num >= option
