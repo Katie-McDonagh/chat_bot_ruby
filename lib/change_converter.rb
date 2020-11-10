9f91feb474ea05feadb2823e8ef821164424b682
Tim <timcpb@hotmail.com>
2020-11-10 16:55:43 +0000

    green test - returns change below £1

--- lib/change_converter.rb
+++ lib/change_converter.rb
@@ -1,13 +1,23 @@
+require 'bigdecimal'
+
 class ChangeConverter
     
     def convert(num)
         results_array = []
-        change_denominations = [50, 20, 10, 5, 2, 1]
+        change_denominations = [50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
         while num > 0 do
             change_denominations.each do |option|
-                if num >= option
+                if num >= option && option >= 1
                     results_array.push("£#{option}")
                     num -= option
+                elsif num >= option && option < 1
+                    bigD = option.round(2)
+                    results_array.push("#{(bigD * 100).to_i}p")
+                    p bigD
+                    p results_array
+                    bigNum = num.round(2)
+                    num = (bigNum - bigD).round(2)
+                    p num
                 end
             end
         end
